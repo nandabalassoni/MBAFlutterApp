d7cc1b8fe0a5ed942077a88c6c26d1cb99bd67ca
Vinicius Taparosky <viniciustaparosky@gmail.com>
2024-04-14 16:16:24 -0300

    Removed old SettingsScreen class

--- lib/setting/settings_screen.dart
+++ lib/setting/settings_screen.dart
@@ -4,52 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-/*class SettingsScreen extends StatelessWidget {
-
-  const SettingsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-
-          Container(
-            margin: EdgeInsets.only(left: 20, top: 40, right: 20, bottom: 20),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                labelText: 'Cotação do dolar em R\$',
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide.none
-                ),
-                prefixIcon: Icon(Icons.monetization_on_outlined),
-              ),
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-            ),
-          ),
-
-          Container(
-            margin: EdgeInsets.all(20),
-            child: TextField(
-              decoration: InputDecoration(
-                  filled: true,
-                  labelText: 'IOF (\%)',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      borderSide: BorderSide.none
-                  ),
-                  prefixIcon: Icon(Icons.money_off_outlined),
-              ),
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-            ),
-          ),
-        ]
-    );
-  }
-}*/
-
 class SettingsWidget extends StatefulWidget{
   const SettingsWidget({super.key});
 
