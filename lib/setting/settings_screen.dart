17e2aaf23bd3a7b24515cc8e72988c4e104c6e90
Vinicius Taparosky <55306962+taparosky@users.noreply.github.com>
2024-04-14 23:36:45 -0300

    Update lib/setting/settings_screen.dart
    Co-authored-by: Bruno Silva <94bruno.silva@gmail.com>

--- lib/setting/settings_screen.dart
+++ lib/setting/settings_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class SettingsWidget extends StatefulWidget{
+class SettingScreen extends StatefulWidget{
   const SettingsWidget({super.key});
 
   @override
